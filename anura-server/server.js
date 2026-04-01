6bbf8d357d2b58909b2a0ffd06e9990a7ac2448e
PatoRosete <kakikerp@gmail.com>
2026-03-31 19:23:30 -0600

    Agregue los archivos de backend

---
+++ anura-server/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const mysql = require('mysql2');
+const app = express()
+const port = 3000
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cors())
+
+// app.js
+
+
+// Create a connection to the database
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'Cerplirp&130506',
+    database: 'anura'
+});
+
+// Connect to the database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('Connected to MySQL Database!');
+
+});
+
+app.listen(port, () => { 
+    console.log(`Anura server running on port ${port}`); 
+});
