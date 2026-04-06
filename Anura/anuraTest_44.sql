a2f3916d787d6fb5317afbd40e53b3888f8d7f99
Em1Torres <emiliotorrescastillo06@gmail.com>
2026-04-05 21:52:15 -0600

    Probando en la base de datos issue 44

--- Anura/anuraTest_44.sql
+++ Anura/anuraTest_44.sql
@@ -1,7 +1,20 @@
 #Corroborar issue 44
+SELECT * FROM anura.users;
+SELECT * FROM anura.playable_character;
 
-INSERT INTO cards (card_name, card_type, effect_value, card_rank)
-VALUES ('Sticky tongue', 'combat', 25, 3),
-('Volley legs', 'movement',,2),
-('Shiny skin', 'utility',,1);
+INSERT INTO cards (card_name, card_type, effect_value)
+VALUES ('Sticky tongue', 'combat', 25),
+('Volley legs', 'movement',0),
+('Shiny skin', 'utility',0),
+('Reni attack','combat',30);
+
+INSERT INTO user_character (uc_user_id,uc_character_id)
+VALUES (2,1),(3,1);
+
+INSERT INTO character_cards (cc_card_id, cc_user_character_id, slot_number, card_rank)
+VALUES (1,1,1,12),(2,1,2,5), (3,2,3,13);
+
+SELECT * FROM anura.character_cards WHERE cc_user_character_id = 1; #usuario de renata y sus cartas de manera general
+
+
 
