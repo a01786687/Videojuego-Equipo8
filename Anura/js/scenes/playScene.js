19a3d07a05f279e98c1bb962657db693dcda2ebe
Em1Torres <emiliotorrescastillo06@gmail.com>
2026-04-06 23:40:01 -0600

    Implementacion final HUD vida (issue 14)

--- Anura/js/scenes/playScene.js
+++ Anura/js/scenes/playScene.js
@@ -32,7 +32,7 @@ let enemies = [];
 
 // --- GLOBAL VARIABLES ---
 let swampSurfaceBg = new Image();
-swampSurfaceBg.src = "/Anura/assets/swamp_surface/swamp_surface_background.png";
+swampSurfaceBg.src = "../Anura/assets/swamp_surface/swamp_surface_background.png";
 
 // Frog object
 let frog = {
@@ -381,10 +381,42 @@ function checkFrogEnemyCollisions(deltaTime) {
         }
     });
 }
+function drawHeart(ctx, x, y, size) {
+    ctx.fillStyle = "red";
+    let s = size; 
+
+    ctx.fillRect(x + s, y, s, s);
+    ctx.fillRect(x + 3*s, y, s, s);
+
+    ctx.fillRect(x, y + s, s, s);
+    ctx.fillRect(x + s, y + s, s, s);
+    ctx.fillRect(x + 2*s, y + s, s, s);
+    ctx.fillRect(x + 3*s, y + s, s, s);
+    ctx.fillRect(x + 4*s, y + s, s, s);
+
+    ctx.fillRect(x, y + 2*s, s, s);
+    ctx.fillRect(x + s, y + 2*s, s, s);
+    ctx.fillRect(x + 2*s, y + 2*s, s, s);
+    ctx.fillRect(x + 3*s, y + 2*s, s, s);
+    ctx.fillRect(x + 4*s, y + 2*s, s, s);
+
+    ctx.fillRect(x + s, y + 3*s, s, s);
+    ctx.fillRect(x + 2*s, y + 3*s, s, s);
+    ctx.fillRect(x + 3*s, y + 3*s, s, s);
+
+    ctx.fillRect(x + 2*s, y + 4*s, s, s);
+}
+function drawHealthBar(ctx){
+    ctx.fillStyle = "orange";
+    
+    ctx.fillRect((canvasWidth/16)-20,(canvasHeight-90)-5,2*currentHealth, 20);
+}
 
 function HealthBarDisplay(){
-    HP_display = new TextLabel(canvasWidth/16,canvasHeight-90,"50px Ubuntu Mono","cyan");
-    HP_display.draw(ctx,currentHealth);
+    drawHeart(ctx,(canvasWidth/16)-15,(canvasHeight-90)+20,10);
+    HP_display = new TextLabel(20,canvasHeight-90,"80spx Ubuntu Mono","cyan");
+    HP_display.draw(ctx,'%'+currentHealth);
+    drawHealthBar(ctx);
 }
 
 // placeholder for the health HUD RF-14
